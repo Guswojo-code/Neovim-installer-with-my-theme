bbfe3b6b198b052e3fc92218afb8e648d39a1bd6
Guswojo-code <74015971+Guswojo-code@users.noreply.github.com>
2023-03-17 15:00:49 +0100

    Add files via upload

---
+++ nvim-installer.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+pacman -S neovim
+echo "Downloaded base vim"
+mkdir "~/.config/"
+echo "Made the config file!"
+mkdir "~/.config/nvim"
+echo "Made the neovim config"
+cd "~/.config/nvim/"
+git clone "~/.config/nvim"
+echo "Downloaded the Neovim config!"
+echo "Done!"
